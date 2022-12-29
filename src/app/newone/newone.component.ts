24043506301bd3edea89c631fbd7bb92d589b383
Lai Yan Hong <icychillerssg@gmail.com>
2022-12-29 17:15:01 +0800

    End of Day 3
    Includes Homework Completed

--- src/app/newone/newone.component.ts
+++ src/app/newone/newone.component.ts
@@ -26,4 +26,42 @@ export class NewoneComponent {
       "date":1
     }
   ]
+
+  game = "efefe"
+
+  color = 'blue'
+  backColor = 'black'
+  padding = '1px'
+  margin = '30px'
+  border = '3px yellow solid'
+  classValue = 'c d'
+  classValue2 = 'b d'
+  ternaryClass = 2
+ /**
+  * name
+  */
+ public callMe() {
+    return 'b d'
+  }
+  imgsrc = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+  alttext = "Google logo"
+
+  multiStyleObj = {
+    color : "red",
+    border : "3px blueviolet solid",
+    display: "inline"
+  }
+  num = 2
+  
+  clickMe(){
+    window.alert("Test")
+  }
+
+  clickMe2(a:Number, b:Number, c:Number){
+    window.alert(a)
+  }
+
 }
+
+
+
